09c4474671695f80f37448c941302d6b474ea658
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-23 22:37:54 +0900

    バックエンドの準備

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__, static_folder='../dist/static', template_folder='../dist')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
